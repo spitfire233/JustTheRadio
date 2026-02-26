87a6c1d99578368a9a1214c9e83243f24ee20422
spitfire233 <87020972+spitfire233@users.noreply.github.com>
2026-02-26 01:18:57 +0100

    Removed useless exception

--- app/src/main/java/com/justtheradio/util/exceptions/NoEndpointFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.justtheradio.util.exceptions;
-
-public class NoEndpointFoundException extends RuntimeException {
-    public NoEndpointFoundException(String message) {
-        super(message);
-    }
-}
--- app/src/main/java/com/justtheradio/util/source/ServiceLocator.java
+++ app/src/main/java/com/justtheradio/util/source/ServiceLocator.java
@@ -1,18 +1,5 @@
 package com.justtheradio.util.source;
 
-import static com.justtheradio.util.constants.Constants.AGENT;
-import static com.justtheradio.util.constants.Constants.NO_ENDPOINT_FOUND_ERROR_MESSAGE;
-import static com.justtheradio.util.constants.Constants.TIMEOUT;
-
-import com.justtheradio.util.exceptions.NoEndpointFoundException;
-
-import java.io.IOException;
-import java.util.Optional;
-
-import de.sfuhrm.radiobrowser4j.ConnectionParams;
-import de.sfuhrm.radiobrowser4j.EndpointDiscovery;
-import de.sfuhrm.radiobrowser4j.RadioBrowser;
-
 public class ServiceLocator {
 
     private static volatile ServiceLocator INSTANCE = null;
