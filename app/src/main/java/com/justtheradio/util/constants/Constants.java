1d0c68aeafd4033eb762521f2139212b78d953a0
spitfire233 <87020972+spitfire233@users.noreply.github.com>
2026-03-18 12:46:32 +0100

    Removed unused constant

--- app/src/main/java/com/justtheradio/util/constants/Constants.java
+++ app/src/main/java/com/justtheradio/util/constants/Constants.java
@@ -4,7 +4,6 @@ public class Constants {
     public static final String AGENT = "JustTheRadio_app";
     public static final int TIMEOUT = 5000; // ms
     public static final int NUMBER_OF_RETRIES = 2;
-    public static final String ERROR_IN_BUILDING_RADIO_BROWSER = "Error in building the radio browser";
     public static final String ERROR_IN_DISCOVERING_ENDPOINT = "Error while trying to discovering an API endpoint!";
     public static final int RADIO_STATIONS_DATABASE_VERSION = 1;
     public static final String RADIO_STATIONS_DATABASE_NAME = "radio_stations_local_db";
